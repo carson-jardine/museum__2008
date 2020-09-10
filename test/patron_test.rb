877d07d511c1f2ed2bc0a4c34320a691e0f97c86
Carson Jardine <carsonjardine@gmail.com>
2020-09-10 09:11:01 -0600

    Write attributes test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -11,14 +11,13 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, @patron_1
   end
 
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+  end
+
 end
-# => #<Patron:0x00007fcb13b5c7d8...>
 
-# @patron_1.name
-# => "Bob"
-
-# @patron_1.spending_money
-# => 20
 
 # @patron_1.interests
 # => []
