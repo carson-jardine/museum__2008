2bea4a5ed84aab4e7120f0287cf5bac8cebef535
Carson Jardine <carsonjardine@gmail.com>
2020-09-10 09:58:42 -0600

    Write code to pass patrons by exhibit test. Needs refactoring

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,9 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -13,5 +14,26 @@ class Museum
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    patrons_by_interests = {}
+    @exhibits.flat_map do |exhibit|
+      patrons_by_interests[exhibit] = []
+      @patrons.select do |patron|
+        if patron.interests.include?(exhibit.name)
+          if patrons_by_interests[exhibit]
+            patrons_by_interests[exhibit] << patron
+          else
+            patrons_by_interests[exhibit] = [patron]
+          end
+        end
+      end
+    end
+    patrons_by_interests
   end
 end
