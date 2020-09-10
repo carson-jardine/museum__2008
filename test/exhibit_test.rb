b9032f2be65a057788d7d3a98c9805e60a679127
Carson Jardine <carsonjardine@gmail.com>
2020-09-10 09:06:29 -0600

    Write attributes test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -10,9 +10,13 @@ class ExhibitTest < Minitest::Test
   def test_it_exists
     assert_instance_of Exhibit, @exhibit
   end
+
+  def test_it_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost 
+  end
 end
 
-# => #<Exhibit:0x00007fcb13bd22d0...>
 
 # @exhibit.name
 # => "Gems and Minerals"
